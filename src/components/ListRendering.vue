adfb1dacdb00d3dc937b82a851947193a0ff7efb
HaeRheeNam <4ran0318@gmail.com>
2024-06-02 01:50:50 +0900

    feat(#6) : 조회 - 리스트 렌더링

---
+++ src/components/ListRendering.vue
@@ -0,0 +1,22 @@
+<script setup>
+const title = "Vue Study";
+const items = [
+  { id: 1, content: "뷰 공부하기" },
+  { id: 2, content: "투두리스트 만들기" },
+];
+</script>
+<template>
+  <main>
+    <h1>
+      {{ title }}
+    </h1>
+    <ul v-for="item in items" :key="item.id">
+      <li>
+        {{ console.log(item.id) }}
+        {{ item.id + ". " + item.content }}
+      </li>
+    </ul>
+  </main>
+</template>
+
+<style></style>
